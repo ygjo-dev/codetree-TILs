b048f011a11720988f52a109a28e560f33703f85
ygjo-dev <ygjo0gon@gmail.com>
2024-10-02 22:30:05 +0900

    Title: 알파벳에 따른 평가, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241002/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,14 @@
+w=input()
+
+if w=="S":
+    print("Superior")
+elif w=="A":
+    print("Excellent")
+elif w=="B":
+    print("Good")
+elif w=="C":
+    print("Usually")
+elif w=="D":
+    print("Effort")
+else:
+    print("Failure")

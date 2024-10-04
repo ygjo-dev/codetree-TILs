a47db416de45ed2b5727de98e20a0f4a02723e53
ygjo-dev <ygjo0gon@gmail.com>
2024-10-04 20:55:24 +0900

    Title: 좌우 지그재그 출력, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241004/좌우 지그재그 출력/print-lr-zigzag.py
@@ -0,0 +1,13 @@
+n=int(input())
+
+cnt=0
+for i in range(n):
+    for j in range(n):
+        if i%2==0:
+            cnt+=1
+            print(cnt,end=" ")
+        if i%2!=0:
+            if j==0:
+                cnt *=2
+            print(cnt-j,end=" ")
+    print()

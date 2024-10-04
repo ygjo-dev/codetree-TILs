efa9f1e485ce2f66a86c5956d2282f85c89b52a5
ygjo-dev <ygjo0gon@gmail.com>
2024-10-04 21:07:31 +0900

    Title: 구구단 만들기 2, Time: 101ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241004/구구단 만들기 2/print-multiplication-table-2.py
@@ -0,0 +1,9 @@
+a,b=input().split()
+a,b=int(a),int(b)
+
+for i in range(2,9,2):
+    for j in range(b,a-1,-1):
+        print(f"{j} * {i} = {j*i}", end=" ")
+        if j!=a:
+            print("/", end=" ")
+    print()

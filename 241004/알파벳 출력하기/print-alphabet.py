23e722878397f4bf428ad14c9fa7ca1100cca3f2
ygjo-dev <ygjo0gon@gmail.com>
2024-10-04 22:40:08 +0900

    Title: 알파벳 출력하기, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241004/알파벳 출력하기/print-alphabet.py
+++ 241004/알파벳 출력하기/print-alphabet.py
@@ -4,8 +4,7 @@ cnt='A'
 for i in range(n):
     for j in range(i+1):
         print(cnt,end="")
-        cnt=chr(ord(cnt)+1)
         if cnt=='Z':
-            print(cnt,end="")
             cnt='A'
+        cnt=chr(ord(cnt)+1)
     print()

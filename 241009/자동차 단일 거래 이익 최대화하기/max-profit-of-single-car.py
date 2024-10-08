cdfd19e9fa05153c38604ae5b941418265f04bbe
ygjo-dev <ygjo0gon@gmail.com>
2024-10-09 00:45:10 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241009/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
@@ -0,0 +1,23 @@
+import sys
+n=int(input())
+arr=list(map(int,input().split()))
+
+min_idx=0
+min_val=sys.maxsize
+
+for idx in range(n):
+    if arr[idx] < min_val:
+        min_val=arr[idx]
+        min_idx=idx
+
+max_idx=0
+max_val=-sys.maxsize
+for idx in range(min_idx+1,n):
+    if arr[idx] > max_val:
+        max_val=arr[idx]
+        max_idx=idx
+
+if max_idx>min_idx:
+    print(max_val-min_val)
+else:
+    print(0)

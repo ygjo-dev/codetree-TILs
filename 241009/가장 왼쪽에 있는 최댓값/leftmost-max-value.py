266707b87bb2903c07d00805e2e1ca986db9d541
ygjo-dev <ygjo0gon@gmail.com>
2024-10-09 00:32:59 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241009/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
@@ -0,0 +1,15 @@
+N=int(input())
+arr=list(map(int,input().split()))
+
+max_idx=N
+while True:
+    max_val=arr[0]
+    for n in arr[:max_idx]:
+        if n>max_val:
+            max_val=N
+    
+    max_idx=arr.index(max_val)
+
+    print(max_idx+1, end=" ")
+    if (max_idx+1)==1:
+        break

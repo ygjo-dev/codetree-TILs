2bef591a12c0c2f8b11a341dd38f698339d28ed2
ygjo-dev <ygjo0gon@gmail.com>
2024-10-16 20:55:42 +0900

    Title: 문자열 계속 지우기, Time: 142ms, Memory: 73MB, Status: Passed - CodeTree

--- 241016/문자열 계속 지우기/keep-removing-string.py
+++ 241016/문자열 계속 지우기/keep-removing-string.py
@@ -5,7 +5,7 @@ while True:
     have_deleted=False
 
     for idx in range(len(A)-len(B)+1):
-        if A[idx:idx+len(B)]==B[::]:
+        if A[idx:idx+len(B)]==B:
             A=A[:idx]+A[idx+len(B):]
             have_deleted=True
 
